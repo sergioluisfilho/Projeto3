ed6a830f82754083f6223a13f44fa8a3296a92e9
Sérgio Luís de Azevedo Cruz Filho <sergioluisfilho@hotmail.com>
2021-05-10 22:50:11 -0300

    Bottom navigator, and screen navigator

---
+++ android/app/src/main/kotlin/com/example/winest/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.winest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:winest/HomePage.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Winest',
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
