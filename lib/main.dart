f9906a4ffa0c66a0a3844e5d044824441aeca3c3
Lucas <lucas.falcao@liferay.com>
2021-05-17 02:35:30 -0300

    Adding LoginScreen and Firebase Auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,30 @@
+import 'dart:html';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:winest/HomePage.dart';
+//import 'package:winest/HomePage.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: 'Winest',
-    home: HomePage(),
-    debugShowCheckedModeBanner: false,
-  ));
+
+  runApp(MyApp());
+
+  Firestore.instance.collection("col").document("doc").setData({"texto": "Lucas"});
+
+  //runApp(MaterialApp(
+  //  title: 'Winest',
+  //  home: HomePage(),
+  //  debugShowCheckedModeBanner: false,
+  //));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Winest",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Container(),
+    );
+  }
 }
