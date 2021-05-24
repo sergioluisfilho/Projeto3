307eb270d7bfaf361fb9afe6d71fe7575b7b8889
Lucas <lucas.falcao@liferay.com>
2021-05-23 22:51:45 -0300

    Adding Login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 //import 'package:winest/HomePage.dart';
+import 'package:winest/login.dart';
 
 void main() {
 
   WidgetsFlutterBinding.ensureInitialized();
 
-  Firestore.instance.collection("Users").document("pontos").setData({"Junior" : "324", "Rafael" : "233"});
+  //Firestore.instance.collection("Users").document("pontos").setData({"Junior" : "324", "Rafael" : "233"});
 
-  runApp(MyApp());
+  //runApp(MyApp());
 
-  //runApp(MaterialApp(
-  //  title: 'Winest',
-  //  home: HomePage(),
-  //  debugShowCheckedModeBanner: false,
-  //));
+  runApp(MaterialApp(
+    title: 'Winest',
+    home: Login(),
+    debugShowCheckedModeBanner: false,
+  ));
 }
 
 class MyApp extends StatelessWidget {
