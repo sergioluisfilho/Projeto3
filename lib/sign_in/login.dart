99d9dc72a8ac6cb5be28e19ffd67a8a3b34a24fb
Sérgio Luís de Azevedo Cruz Filho <sergioluisfilho@hotmail.com>
2021-06-03 23:17:38 -0300

    Update login.dart

--- lib/sign_in/login.dart
+++ lib/sign_in/login.dart
@@ -19,81 +19,87 @@ class _LoginState extends State<Login> {
     return Scaffold(
       backgroundColor: Color.fromRGBO(92, 17, 94, 2),
       // appBar: AppBar(title: Text("Login")),
-      body: Container(
-        padding: EdgeInsets.only(top: 20),
-        child: _isLoggedIn
-            ? Center(
-                child: Column(
-                  // crossAxisAlignment: CrossAxisAlignment.stretch,
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.only(top: 20),
+          child: _isLoggedIn
+              ? Center(
+                  child: Column(
+                    // crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Image.network(_userObj.photoUrl),
+                      Text(_userObj.displayName),
+                      Text(_userObj.email),
+                      TextButton(
+                          onPressed: () {
+                            _googleSignIn.signOut().then((value) {
+                              setState(() {
+                                _isLoggedIn = false;
+                              });
+                            }).catchError((e) {});
+                          },
+                          child: Text("Logout"))
+                    ],
+                  ),
+                )
+              : Column(
+                  // crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Image.network(_userObj.photoUrl),
-                    Text(_userObj.displayName),
-                    Text(_userObj.email),
-                    TextButton(
+                    Image.asset('images/logo.png'),
+                    Center(
+                      // ignore: deprecated_member_use
+                      child: OutlineButton.icon(
+                        label: Text(
+                          'Sign In With Google',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.white),
+                        ),
+                        shape: StadiumBorder(),
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                        highlightedBorderColor: Colors.black,
+                        borderSide: BorderSide(color: Colors.white),
+                        textColor: Colors.black,
+                        icon:
+                            FaIcon(FontAwesomeIcons.google, color: Colors.red),
                         onPressed: () {
-                          _googleSignIn.signOut().then((value) {
+                          _googleSignIn.signIn().then((userData) {
                             setState(() {
-                              _isLoggedIn = false;
+                              _isLoggedIn = true;
+                              _userObj = userData;
+                              Navigator.push(
+                                  context,
+                                  new MaterialPageRoute(
+                                      builder: (contex) => HomePage()));
                             });
-                          }).catchError((e) {});
+                          }).catchError((e) {
+                            print(e);
+                            showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return AlertDialog(
+                                    title: Text("LOGIN ERROR"),
+                                  );
+                                });
+                          });
                         },
-                        child: Text("Logout"))
-                  ],
-                ),
-              )
-            : Column(
-                // crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Image.asset('images/logo.png'),
-                  Center(
-                    child: OutlineButton.icon(
-                      label: Text(
-                        'Sign In With Google',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 20),
                       ),
-                      shape: StadiumBorder(),
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-                      highlightedBorderColor: Colors.black,
-                      borderSide: BorderSide(color: Colors.black),
-                      textColor: Colors.black,
-                      icon: FaIcon(FontAwesomeIcons.google, color: Colors.red),
-                      onPressed: () {
-                        _googleSignIn.signIn().then((userData) {
-                          setState(() {
-                            _isLoggedIn = true;
-                            _userObj = userData;
+                    ),
+                    Center(
+                      child: ElevatedButton(
+                          child: Text("Create account"),
+                          onPressed: () {
                             Navigator.push(
                                 context,
                                 new MaterialPageRoute(
-                                    builder: (contex) => HomePage()));
-                          });
-                        }).catchError((e) {
-                          print(e);
-                          showDialog(
-                              context: context,
-                              builder: (BuildContext context) {
-                                return AlertDialog(
-                                  title: Text("LOGIN ERROR"),
-                                );
-                              });
-                        });
-                      },
-                    ),
-                  ),
-                  Center(
-                    child: ElevatedButton(
-                        child: Text("Create account"),
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              new MaterialPageRoute(
-                                  builder: (contex) => SignUpPage()));
-                        }),
-                  )
-                ],
-              ),
+                                    builder: (contex) => SignUpPage()));
+                          }),
+                    )
+                  ],
+                ),
+        ),
       ),
     );
   }
